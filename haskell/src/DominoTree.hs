5e5eec23d4c2fc926af1036a3d39fbd116296f6c
Erik Huizinga <huizinga.erik@gmail.com>
2017-10-10 10:16:47 +0200

    Stub various bone (data) types

--- haskell/src/DominoTree.hs
+++ haskell/src/DominoTree.hs
@@ -18,15 +18,20 @@ type BoneNumber = Int
 
 type BonePosition = (Position, Position)
 
-type Bone = (BonePips, BoneNumber)
+type UnpositionedBoneType = (BonePips, BoneNumber)
 
-type PositionedBone = (Bone, BonePosition)
+type PositionedBoneType = (UnpositionedBoneType, BonePosition)
+
+data Bone
+  = PositionedBone PositionedBoneType
+  | UnpositionedBone UnpositionedBoneType
+  deriving (Show)
 
 type BoneTree = Tree Bone
 
 data PuzzleContent
   = PuzzlePips Pips
-  | PlacedBone PositionedBone
+  | PlacedBone PositionedBoneType
 
 type Puzzle = [PuzzleContent]
 
@@ -38,9 +43,12 @@ input1 = [PuzzlePips p | p <- [0, 0, 0, 1, 1, 1]]
 
 initialBones :: [Bone]
 initialBones =
-  zip
-    [(pips1, pips2) | pips1 <- [0 .. maxPips], pips2 <- [pips1 .. maxPips]] -- ^ 'BonePips'
-    [1 ..] -- ^ 'BoneNumber'
+  [ UnpositionedBone unpositionedBone
+  | unpositionedBone <-
+      zip
+        [(pips1, pips2) | pips1 <- [0 .. maxPips], pips2 <- [pips1 .. maxPips]] -- ^ 'BonePips'
+        [1 ..] -- ^ 'BoneNumber'
+  ]
 
 -- | The maximum row index in the puzzle grid
 maxRow :: Index
@@ -53,4 +61,21 @@ maxColumn = maxPips
 isValidPosition :: Position -> Bool
 isValidPosition (row, column) = row >= 0 && column >= 0 && row <= maxRow && column <= maxColumn
 
+findAvailableBonesWithPips ::
+     Pips
+  -> [Bone] -- ^ Available 'Bone' instances
+  -> [Bone]
+findAvailableBonesWithPips _ []              = []
+findAvailableBonesWithPips pips (bone:bones) = [] -- TODO
 
+arePipsOnBone :: Pips -> Bone -> Bool
+arePipsOnBone pips bone =
+  case bone of
+    (PositionedBone (unpositionedBone, _)) -> arePipsOnUnpositionedBone pips unpositionedBone
+    (UnpositionedBone unpositionedBone) -> arePipsOnUnpositionedBone pips unpositionedBone
+
+arePipsOnUnpositionedBone :: Pips -> UnpositionedBoneType -> Bool
+arePipsOnUnpositionedBone pips (bonePips, _) = elemPipsBonePips pips bonePips
+
+elemPipsBonePips :: Pips -> BonePips -> Bool
+elemPipsBonePips pips (bonePips1, bonePips2) = pips `elem` [bonePips1, bonePips2]
