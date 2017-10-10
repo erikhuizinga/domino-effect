ff164390d2eb45050d84e6f6ee994a56fba51dee
Erik Huizinga <huizinga.erik@gmail.com>
2017-10-10 17:34:58 +0200

    Extend prep for recursion

--- haskell/src/DominoRecurse.hs
+++ haskell/src/DominoRecurse.hs
@@ -3,22 +3,42 @@ module DominoRecurse where
 dominoRecurse :: IO ()
 dominoRecurse = do
   putStrLn "Domino Recurse"
-  solutions <- recurseIO input0
+  let solutions = recurse input0 initialPositions initialBones [initialSolution input0]
   print solutions
   return ()
 
 maxPips :: Int
 maxPips = 0
 
+input0 :: [Int]
 input0 = [0, 0]
 
+input1 :: [Int]
 input1 = [0, 0, 0, 1, 1, 1]
 
 initialBones :: [((Int, Int), Int)]
 initialBones = zip [(pips1, pips2) | pips1 <- [0 .. maxPips], pips2 <- [pips1 .. maxPips]] [1 ..]
 
-recurseIO :: [Int] -> IO [Int]
-recurseIO xs = return $ recurse xs
+initialPositions :: [(Int, Int)]
+initialPositions = [(r, c) | r <- [1 .. maxRow], c <- [1 .. maxColumn]]
 
-recurse :: [Int] -> [Int]
-recurse (x:xs) = []
+initialSolution :: [Int] -> [Int]
+initialSolution = map (const 0)
+
+maxRow :: Int
+maxRow = maxPips + 1
+
+maxColumn :: Int
+maxColumn = maxRow + 1
+
+numBones :: Int
+numBones = sum [1 .. maxPips + 1]
+
+recurse ::
+     [Int] -- ^ Numbers to place bones on
+  -> [(Int, Int)] -- ^ Set of available positions
+  -> [((Int, Int), Int)] -- ^ Set of available bones
+  -> [[Int]] -- ^ Current solutions
+  -> [[Int]] -- ^ Solutions
+recurse _ [] [] solutions                 = solutions
+recurse (n:ns) (pos:poss) bones solutions = []
