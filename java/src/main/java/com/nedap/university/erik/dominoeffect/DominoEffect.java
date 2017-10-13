d1342b99f05a32aca82ae357311bba5d2e6ce0d9
Erik Huizinga <huizinga.erik@gmail.com>
2017-10-13 19:19:04 +0200

    Update application

--- java/src/main/java/com/nedap/university/erik/dominoeffect/DominoEffect.java
+++ java/src/main/java/com/nedap/university/erik/dominoeffect/DominoEffect.java
@@ -1,11 +1,22 @@
 package com.nedap.university.erik.dominoeffect;
 
 import com.nedap.university.erik.dominoeffect.Puzzles.Assignment1;
+import java.util.Set;
 
 /** Created by erik.huizinga on 8-10-17 */
 public class DominoEffect {
   public static void main(String[] args) {
     Puzzle puzzle = new Assignment1();
-    System.out.println(puzzle.print(puzzle.getMaxPips()));
+    int maxPips = puzzle.getMaxPips();
+
+    System.out.println("Domino Effect");
+    System.out.println();
+
+    System.out.println("Bones:");
+    Set<Bone> bones = Bone.initialSetOf(maxPips);
+    System.out.println(Bone.print(bones, maxPips));
+    System.out.println();
+
+    System.out.println(puzzle.print(maxPips));
   }
 }
