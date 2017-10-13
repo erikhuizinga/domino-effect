50842e678a39d4b865a3afdefaf6ebafc469ca33
Erik Huizinga <huizinga.erik@gmail.com>
2017-10-13 19:11:16 +0200

    Refactor Bone to SingletonMap and Comparable

--- java/src/main/java/com/nedap/university/erik/dominoeffect/Bone.java
+++ java/src/main/java/com/nedap/university/erik/dominoeffect/Bone.java
@@ -1,11 +1,125 @@
 package com.nedap.university.erik.dominoeffect;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /** Created by erik.huizinga on 13-10-17 */
-public class Bone {
+public class Bone implements Map<Integer, Set<Integer>>, Comparable {
+
+  private final Map<Integer, Set<Integer>> map;
+  private final int boneNumber;
+  private final int pips0;
+  private final int pips1;
+
+  public Bone(Integer pips0, Integer pips1, int boneNumber) {
+    // put(boneNumber, new HashSet<>(Arrays.asList(pips0, pips1)));
+    map =
+        Collections.singletonMap(
+            boneNumber, Collections.unmodifiableSet(new HashSet<>(Arrays.asList(pips0, pips1))));
+    this.pips0 = pips0;
+    this.pips1 = pips1;
+    this.boneNumber = boneNumber;
+  }
 
   public static int totalNumber(int maxPips) {
     return IntStream.rangeClosed(1, maxPips + 1).sum();
+  }
+
+  public static SortedSet<Bone> initialSetOf(int maxPips) {
+    SortedSet<Bone> bones = new TreeSet<>();
+    int boneNumber = 1;
+    for (Integer pips0 = 0; pips0 <= maxPips; pips0++) {
+      for (Integer pips1 = pips0; pips1 <= maxPips; pips1++) {
+        bones.add(new Bone(pips0, pips1, boneNumber));
+        boneNumber++;
+      }
+    }
+    return bones;
+  }
+
+  @Override
+  public int compareTo(Object obj) {
+    if (obj instanceof Bone) {
+      Bone that = (Bone) obj;
+      return boneNumber - that.boneNumber;
+    }
+    throw new ClassCastException("Not a Bone");
+  }
+
+  @Override
+  public int size() {
+    return map.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return map.isEmpty();
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    return map.containsKey(key);
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    return map.containsValue(value);
+  }
+
+  @Override
+  public Set<Integer> get(Object key) {
+    return map.get(key);
+  }
+
+  @Override
+  public Set<Integer> put(Integer key, Set<Integer> value) {
+    return map.put(key, value);
+  }
+
+  @Override
+  public Set<Integer> remove(Object key) {
+    return map.remove(key);
+  }
+
+  @Override
+  public void putAll(Map<? extends Integer, ? extends Set<Integer>> m) {
+    map.putAll(m);
+  }
+
+  @Override
+  public void clear() {
+    map.clear();
+  }
+
+  @Override
+  public Set<Integer> keySet() {
+    return map.keySet();
+  }
+
+  @Override
+  public Collection<Set<Integer>> values() {
+    return map.values();
+  }
+
+  @Override
+  public Set<Entry<Integer, Set<Integer>>> entrySet() {
+    return map.entrySet();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Bone) {
+      Bone that = (Bone) obj;
+      return pips0 == that.pips0 && pips1 == that.pips1 && boneNumber == that.boneNumber;
+    }
+    return super.equals(obj);
   }
 }
--- java/src/test/java/com/nedap/university/erik/dominoeffect/BoneTest.java
+++ java/src/test/java/com/nedap/university/erik/dominoeffect/BoneTest.java
@@ -1,11 +1,25 @@
 package com.nedap.university.erik.dominoeffect;
 
+import static com.nedap.university.erik.dominoeffect.TestData.bone1;
+import static com.nedap.university.erik.dominoeffect.TestData.bone2;
+import static com.nedap.university.erik.dominoeffect.TestData.bone3;
+import static com.nedap.university.erik.dominoeffect.TestData.bones1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Collection;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /** Created by erik.huizinga on 13-10-17 */
 class BoneTest {
+
+  @BeforeEach
+  void setUp() {
+    TestData.reset();
+  }
 
   @Test
   void totalNumber() {
@@ -16,5 +30,38 @@ class BoneTest {
     assertEquals(15, Bone.totalNumber(4));
     assertEquals(21, Bone.totalNumber(5));
     assertEquals(28, Bone.totalNumber(6));
+  }
+
+  @Test
+  void initialSetOf() {
+    Set<Bone> bones = Bone.initialSetOf(1);
+    checkEquality(bones1, bones);
+    checkEquality(bones, bones1);
+  }
+
+  /** Why does {@code set1.containsAll(set2) && set2.containsAll(set1)} not work? */
+  private void checkEquality(Collection<Bone> expectedBones, Collection<Bone> actualBones) {
+    boolean ok = false;
+    for (Bone expected : expectedBones) {
+      for (Bone actual : actualBones) {
+        if (expected.equals(actual)) {
+          ok = true;
+          break;
+        }
+      }
+      if (ok) {
+        break;
+      }
+    }
+    if (!ok) {
+      fail("Expected Bone not found");
+    }
+  }
+
+  @Test
+  void compareTo() {
+    assertTrue(bone1.compareTo(bone2) < 0);
+    assertTrue(bone2.compareTo(bone2) == 0);
+    assertTrue(bone3.compareTo(bone2) > 0);
   }
 }
